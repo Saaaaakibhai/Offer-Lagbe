12f8e486408ac22b7d9a1c12098d650c2a1664fe
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-05-02 22:50:19 +0600

    Firabase Intrigate
    Login Sign In and this data saved in firebase realtime

---
+++ Offer Lagbe/app/src/main/java/com/example/offerlagbe/Fragment/User.java
@@ -0,0 +1,44 @@
+package com.example.offerlagbe.Fragment;
+
+public class User {
+    private String name,companyname,email,password;
+
+    public User(String name, String companyname, String email, String password) {
+        this.name = name;
+        this.companyname = companyname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompanyname() {
+        return companyname;
+    }
+
+    public void setCompanyname(String companyname) {
+        this.companyname = companyname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
